582414a02a397bd3e9a1fc5e9b133e14d875bac9
Kenneth Pay <kpay@uchicago.edu>
2019-11-20 17:11:28 -0500

    add more country data

--- Run_MLSS.m
+++ Run_MLSS.m
@@ -1,28 +1,42 @@
 %% Solve for Steady State Currency Values
 clear variables; clc
 
-% Nodes
-n = 5;
+time = [];
+time_jfree = [];
+mem = [];
+mem_jfree = [];
+for i = 1:100
+    % Nodes
+n = 20;
 m = n-1;
-% Australia, Canada, China, EU, India
 % Define system parameters and add them to "Params" structure
-Params.n    = n;             % number of nodes
-Params.m    = n-1;           % number of export flows on each node
-Params.y    = [1.43; 1.71; 13.6; 18.8; 2.73];     % nodal GDPs (billions)
-Params.P    = [118; 115; 122; 111; 168];     % nodal CPIs
-Params.d    = [16000; 9000; 16700; 10300;
-                16000; 10000; 6000; 11000;
-                9000; 10000; 8000; 4000;
-                17000; 6000; 8000; 6000;
-                10000; 11000; 4000; 6000];   % distance vector
-Params.nu   = [0.0482; 0.2327; 0.3051; 0.4518; 
-                1.0006; 0.8715; 0.1926; 1.9027;
-                1.2965; 1.8060; 0.8578; 0.3376;
-                1.5178; 0.6343; 0.4766; 0.7606;
-                1.1529; 0.1558; 1.5679; 0.3409];   % value differential vector
-Params.yw   = 84.4;          % global GDP
-Params.gam1 = ones(n*m,1);   % tuning vector 1
-Params.gam2 = ones(n*m,1);   % tuning vector 2
+ Params.n    = n;             % number of nodes
+ Params.m    = n-1;           % number of export flows on each node
+ Params.y = rand(n,1);
+ Params.P = rand(n,1)*100;
+ Params.d = rand(n*m,1)*10000;
+ Params.nu = rand(n*m,1);
+ Params.yw = rand(1,1)*100;
+ 
+% Australia, Canada, China, EU, India, Chile, Japan, Mexico, Morocco, Poland
+% Params.y    = [1.43; 1.71; 13.6; 18.8; 2.73; 
+%                 0.298; 4.97; 1.22; 0.118; 0.586];     % nodal GDPs (trillions)
+% Params.P    = [117.898; 114.525; 121.559; 111.247; 167.598;
+%                 128.624; 104.981; 136.577; 110.850; 111.625];    % nodal CPIs (2010 = 100)
+%             
+% fileID = fopen('distvec.txt', 'r');
+% formatSpec = '%f';
+% dist = fscanf(fileID, formatSpec);
+% Params.d    = dist;   % distance vector (km)
+% 
+% Params.nu   = [0.0482; 0.2327; 0.3051; 0.4518; 
+%                 1.0006; 0.8715; 0.1926; 1.9027;
+%                 1.2965; 1.8060; 0.8578; 0.3376;
+%                 1.5178; 0.6343; 0.4766; 0.7606;
+%                 1.1529; 0.1558; 1.5679; 0.3409];   % value differential vector
+% Params.yw   = sum(Params.y);          % global GDP
+% Params.gam1 = ones(n*m,1);   % tuning vector 1
+% Params.gam2 = ones(n*m,1);   % tuning vector 2
 
 % Build special incidence matrix E, Lambda, Gamma and A
 [E,Lambda,Gamma] = Build_ELG(Params.n);
@@ -34,14 +48,35 @@ Params.gam2 = ones(n*m,1);   % tuning vector 2
 %% Use loop to drive N to 0 for a given rho:
 Yt  = Y0;
 h = 1e-5; % finite diff paramter for numerical Jacobian.
-for rho = 0.1 : 0.1 : 1
+rho = 0.05;
     
     [N] = MLSS_NonLin(Yt,Gamma,Lambda,A,Params,rho);
-    [newton_sol,k] = Newton(Yt,Gamma,Lambda,A,Params,rho,h)
+    tic; [newton_sol,k, m] = Newton(Yt,Gamma,Lambda,A,Params,rho,h);
+    t = toc;
     fprintf('number of Newton iterations=%d\n',k);
 
-    [newton_sol_jfree,k_jfree] = Newton_JFree(Yt,Gamma,Lambda,A,Params,rho, 1e-09, 1e-5)
+    tic; [newton_sol_jfree,k_jfree, m_jfree] = Newton_JFree(Yt,Gamma,Lambda,A,Params,rho, 1e-02, 1e-5);
+    t_jfree = toc;
     fprintf('number of Newton iterations=%d\n',k_jfree);
 
+    time = [time, t];
+    time_jfree = [time_jfree, t_jfree];
+    mem = [mem, m];
+    mem_jfree = [mem_jfree, m_jfree];
     Yt = newton_sol;
 end
+
+plot(time)
+hold on
+plot(time_jfree)
+hold off
+legend('backslash', 'j free')
+ylabel('time')
+
+plot(mem)
+hold on
+plot(mem_jfree)
+hold off
+legend('backslash', 'j free')
+ylabel('memory')
+
