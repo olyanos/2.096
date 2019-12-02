fef47cc59b3c8539200906f0f14e511209e9b0d8
Omar Alolayan <omar@dhcp-18-20-182-224.dyn.mit.edu>
2019-12-02 15:09:36 -0500

    fixing newton to have a tighter bound

--- Newton.m
+++ Newton.m
@@ -7,7 +7,7 @@ alpha = 1;
 fYt = MLSS_NonLin(Yt,Params,rho);
 dYt = ones(n,1);
 error1 = 1;
-while ( norm (dYt) > 1e-3 & norm (fYt) > 1e-3)
+while ( norm (dYt) > 1e-8 & norm (fYt) > 1e-8)
          fYt = MLSS_NonLin(Yt,Params,rho); % function calculated @ (Yt);
          JYt = compute_Jacobian(Yt,Gamma,Lambda,A,Params,rho,h); % Jacobian calculated @ (Yt);
              
@@ -31,7 +31,7 @@ while ( norm (dYt) > 1e-3 & norm (fYt) > 1e-3)
          else
              Yt = Yt_new;
              error1 = error2;
-             alpha = alpha * 1.2;
+             alpha = alpha * 1.01;
          end
     end
 
